6f677f796f32d1796e40b42c9ffc56fd6b03c321
Figueira <marina.f.figueira@accenture.com>
2019-10-16 08:12:27 -0300

    Projeto Alurapic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app.routing.module';
 
-import { AppRoutingModule } from './app-routing.module';
+import { PhotosModule } from './photos/photos.module';
 import { AppComponent } from './app.component';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PhotosModule,
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
